58f49193dbf96976048a3812a2b669cb796be5c7
Amr El-basuony <45123130+Hiden-Evil@users.noreply.github.com>
2020-10-01 20:16:36 +0200

    some bug fixes

---
+++ app/src/main/java/com/example/ui/models/Group.java
@@ -0,0 +1,13 @@
+package com.example.ui.models;
+
+public class Group {
+    String group;
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
