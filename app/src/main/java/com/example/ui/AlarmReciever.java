bd896364cd8679860eca3ef3fe4e5e243776a35c
Randa <randahada99@gmail.com>
2020-10-01 02:42:15 +0200

    solve notification bug and enter past time

--- app/src/main/java/com/example/ui/AlarmReciever.java
+++ app/src/main/java/com/example/ui/AlarmReciever.java
@@ -1,18 +1,14 @@
 package com.example.ui;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsManager;
 import android.widget.Toast;
-
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ui.Database.LogDB.AddNewViewmodelLog;
 
@@ -72,15 +68,6 @@ public class AlarmReciever extends BroadcastReceiver {
 
             }
             Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
-//            NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
-//            Notification.Builder builder = new Notification.Builder(context);
-//            builder.setSmallIcon(R.drawable.ic_baseline_notifications_24)
-//                    .setContentTitle(result)
-//                    .setContentText(msg)
-//                    .setWhen(System.currentTimeMillis())
-//                    .setAutoCancel(true)
-//                    .setContentIntent(contenetintent);
-//            notificationManager.notify(notificationid, builder.build());
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"channelid")
                     .setSmallIcon(R.drawable.ic_baseline_notifications_24)
                     .setContentTitle(result)
